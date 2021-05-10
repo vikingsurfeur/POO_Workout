ed4592413ea988e0eb6a50e1410d75b1537fbd7c
David Bouscarle <d.bouscarle@gmail.com>
2021-05-10 15:19:48 +0200

    Add a singleton Pattern to Database Class

--- libraries/Database.php
+++ libraries/Database.php
@@ -2,14 +2,19 @@
 
 class Database
 {
-    public static function getPdo(): PDO
-    {
-        $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Attribut une erreur au mode silencieux de mySql
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Attribut un tableau associatif des données
-        ]);
+    private static $instance = null;
 
-        return $pdo;
+    public static function getPdo()
+    {
+        if(is_null(self::$instance))
+        {
+            self::$instance = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Attribut une erreur au mode silencieux de mySql
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Attribut un tableau associatif des données
+            ]);
+        }
+
+        return self::$instance;
     }
 }
 
